7bd15ae4f53d10c194937d3c9f6685f25db530b6
Kyouhei Hayama <khayama@users.noreply.github.com>
2020-02-13 11:59:53 +0900

    Update show_all_table.py

--- show_all_table.py
+++ show_all_table.py
@@ -34,18 +34,14 @@ for i in range(len(avg)):
     latency = avg[i]['value']
     print(str(i) + " : " + sending_zone + " --> " + receiving_zone + " : " + str(latency) + " ms")
 
-zones = ['', 'dal13', 'hou02', 'mex01', 'mon01', 'sea01', 'sjc04', 'tor01', 'wdc07', 'ams03', 'fra05', 'lon02', 'mil01', 'par01', 'che01', 'hkg02', 'mel01', 'sng01', 'syd05', 'tok05', 'sao01']
+zones = ['(ms)', 'dal13', 'hou02', 'mex01', 'mon01', 'sea01', 'sjc04', 'tor01', 'wdc07', 'ams03', 'fra05', 'lon02', 'mil01', 'par01', 'che01', 'hkg02', 'mel01', 'sng01', 'syd05', 'tok05', 'sao01']
 n = len(zones)-1
 array = [[0] * n for i in range(n)]
 for i in range(n):
     for j in range(n):
-        if i < j:
-            array[i][j] = avg.pop(0)['value']
-
-for i in range(n):
-    for j in range(n):
-        if i > j:
-            array[i][j] = avg.pop(0)['value']
+        if i != j:
+            data = [x['value'] for x in avg if x['labels'][435:440] == zones[i+1] and x['labels'][359:364] == zones[j+1] ]
+            array[i][j] = int(data[0])
 
 from prettytable import PrettyTable 
 
